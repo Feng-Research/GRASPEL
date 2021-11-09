9fda31df8c13eeb14131164edaaf499d0ffe08f0
janticev <john.m.anticev@gmail.com>
2021-11-09 13:23:18 -0500

    Delete TEST DEBUG.jl

--- TEST DEBUG.jl
+++
@@ -1,9 +0,0 @@
-using Colors, Plots, LinearAlgebra, MAT
-vars = matread(raw"C:\Users\jacec\Desktop\code\code\USPS.mat")
-plotUSPSfea(A::Matrix,index::Int) = plot(Gray.(transpose(reshape(A[index,:],(16,16)))))
-p = Vector{typeof(plot())}(undef,5)
-for i = 1:5
-    #display(plotUSPSfea(vars["fea"],i))
-    p[i] = plotUSPSfea(vars["fea"],i)
-end
-plot(p...)
